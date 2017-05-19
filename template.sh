88ffc2340a0f24d58297b55b24522dfe0868c7e4
startupgurukul <dheerajkota@gmail.com>
2017-05-19 08:53:34 +0530

    Create template.sh

---
+++ template.sh
@@ -0,0 +1,10 @@
+npm install --save re-base semantic-ui-css semantic-ui-react react-router-dom formsy-react formsy-semantic-ui-react
+cd src
+rm App.js
+wget https://raw.githubusercontent.com/startupgurukul/semantic-ui-cra-react-starter/master/src/App.js
+wget https://raw.githubusercontent.com/startupgurukul/semantic-ui-cra-react-starter/master/src/Home.js
+wget https://raw.githubusercontent.com/startupgurukul/semantic-ui-cra-react-starter/master/src/Login.js
+wget https://raw.githubusercontent.com/startupgurukul/semantic-ui-cra-react-starter/master/src/MyRoutes.js
+wget https://raw.githubusercontent.com/startupgurukul/semantic-ui-cra-react-starter/master/src/constants.js
+cd ../public
+wget https://github.com/startupgurukul/semantic-ui-cra-react-starter/raw/master/public/paragraph.png
